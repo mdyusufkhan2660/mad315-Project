b136d36b2fc3385ac788072c07ecd4b10b20f096
Sharikashaji <sharikashaji3@gmail.com>
2022-11-07 08:49:51 -0500

    Game list design
    I have created the design of a home list page containing different games with images.

---
+++ app/src/main/java/com/example/group_3_project_mad315/Game_home.kt
@@ -0,0 +1,11 @@
+package com.example.group_3_project_mad315
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_home)
+    }
+}
