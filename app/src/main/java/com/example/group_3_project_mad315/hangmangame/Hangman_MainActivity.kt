c2b75fb2cea25e9badfb1d4c0c93b1212f099bef
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-11-08 11:33:47 -0500

    added score to hangman

--- app/src/main/java/com/example/group_3_project_mad315/hangmangame/Hangman_MainActivity.kt
+++ app/src/main/java/com/example/group_3_project_mad315/hangmangame/Hangman_MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hangmangame
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -10,8 +12,14 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import com.example.group_3_project_mad315.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Hangman_MainActivity : AppCompatActivity() {
+    var score = 0
+    lateinit var sharedPreferences: SharedPreferences
+    var editor: SharedPreferences.Editor? = null
+
 
     private val gameManager = GameManager()
 
@@ -74,9 +82,31 @@ class Hangman_MainActivity : AppCompatActivity() {
         wordTextView.text = wordToGuess
         gameWonTextView.visibility = View.VISIBLE
         lettersLayout.visibility = View.GONE
+        score++
     }
 
     private fun startNewGame() {
+
+        //Saving the username in sharedpreference
+        sharedPreferences = getSharedPreferences("mad315", MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+        editor!!.putString("hm_score", score.toString())
+        editor!!.apply()
+        //Getting the username from shared preference
+        Log.d("hm_score: ", sharedPreferences.getString("hm_score","0").toString())
+
+        Firebase.firestore.collection("users").document(sharedPreferences.getString("name","0").toString()).get().addOnSuccessListener {
+                result ->
+            if(result.data!!.getValue("hm_score").toString().equals("")){
+                savePoints(sharedPreferences.getString("name","test")!!, score)
+
+            }
+            else if(Integer.parseInt(result.data!!.getValue("hm_score").toString()) < score){
+                savePoints(sharedPreferences.getString("name","test")!!, score)
+            }
+        }
+
+
         gameLostTextView.visibility = View.GONE
         gameWonTextView.visibility = View.GONE
         val gameState = gameManager.startNewGame()
@@ -85,5 +115,9 @@ class Hangman_MainActivity : AppCompatActivity() {
             letterView.visibility = View.VISIBLE
         }
         updateUI(gameState)
+    }
+
+    fun savePoints(id: String, points: Int) {
+        Firebase.firestore.collection("users").document(id).update("hm_score", points.toString())
     }
 }
