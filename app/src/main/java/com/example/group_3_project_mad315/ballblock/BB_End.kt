690f43516e9af2b68b0fc5fbe2e4e322b0f4bdd8
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-11-08 11:41:41 -0500

    fixed back button bug on ball game end screen

--- app/src/main/java/com/example/group_3_project_mad315/ballblock/BB_End.kt
+++ app/src/main/java/com/example/group_3_project_mad315/ballblock/BB_End.kt
@@ -3,15 +3,13 @@ package com.example.group_3_project_mad315.ballblock
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.group_3_project_mad315.Game_List
 import com.example.group_3_project_mad315.R
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -103,5 +101,10 @@ class End : AppCompatActivity() {
 
     fun savePoints(id: String, points: Int) {
         Firebase.firestore.collection("users").document(id).update("bb_score", points.toString())
+    }
+
+    override fun onBackPressed() {
+        val i = Intent(this@End,Game_List::class.java)
+        startActivity(i)
     }
 }
